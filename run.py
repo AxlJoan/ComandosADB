aef54bf98a8f3164ec8abcdff64cd703e2c466a1
AxlJoan <60158935+AxlJoan@users.noreply.github.com>
2024-12-06 12:07:53 -0500

    Evitar la importación circular x2
    # from app import app

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ def send_command():
 
 
 # Ejecutar el servidor Flask
-from app import app
+#from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
